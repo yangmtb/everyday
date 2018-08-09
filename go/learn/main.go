b35e0f6eaee739c5df0ec4133a12f26534c69d71
YangYang <yangyang@forensix.cn>
2018-08-09 09:54:50 +0800

    update

--- go/learn/main.go
+++ go/learn/main.go
@@ -1,6 +1,11 @@
 package main
 
 import (
+	"fmt"
+	"io/ioutil"
+	"log"
+	"os/exec"
+	"reflect"
 	"sync"
 	"time"
 )
@@ -18,7 +23,7 @@ func (d *data) test(s string) {
 	}
 }
 
-func main() {
+func testLock() {
 	var wg sync.WaitGroup
 	wg.Add(2)
 
@@ -32,4 +37,47 @@ func main() {
 		d.test("write")
 	}()
 	wg.Wait()
+}
+
+type X int
+
+func testReflect() {
+	var a X = 100
+	var d data
+	t := reflect.TypeOf(d)
+	fmt.Println(t, t.Name(), t.Kind(), a)
+	aa := reflect.ArrayOf(10, reflect.TypeOf(byte(0)))
+	m := reflect.MapOf(reflect.TypeOf(""), reflect.TypeOf(0))
+	fmt.Println(aa, m)
+}
+
+func testCPU() {
+	go func() {
+		for {
+
+		}
+	}()
+	time.Sleep(30 * time.Second)
+}
+
+func testdo() {
+	cmd := exec.Command("bash", "echo.sh")
+	//cmd := exec.Command("ls", "-a", "-l")
+	stdout, _ := cmd.StdoutPipe()
+	defer stdout.Close()
+	if err := cmd.Start(); nil != err {
+		log.Fatal(err)
+	}
+	op, err := ioutil.ReadAll(stdout)
+	if nil != err {
+		log.Fatal(err)
+	}
+	fmt.Println("done", string(op))
+}
+
+func main() {
+	//testReflect()
+	//testCPU()
+	testdo()
+	return
 }
